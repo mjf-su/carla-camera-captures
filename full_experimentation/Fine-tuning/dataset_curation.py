75902ccfbe4b5654672f1d11e89437f0637a2a2c
mjf-su <mfoutter@stanford.edu>
2023-12-01 18:52:48 -0800

    experiment wise dataset files

--- full_experimentation/Fine-tuning/dataset_curation.py
+++ full_experimentation/Fine-tuning/dataset_curation.py
@@ -10,15 +10,15 @@ dataset_file = "trim_VQADataset.npz"
 for scenario in scenarios:
     scenario_folder = os.path.join(folder, "data", scenario) # scenario type
 
-    questions = []
-    answers = []
-    filenames = []
-
     with open(os.path.join(scenario_folder, "incorrect_labels.json"), 'r') as js:
         incorrect_labels = json.load(js)
     for exp in os.listdir(scenario_folder):
         if "exp" not in exp:
             continue
+
+        questions = []
+        answers = []
+        filenames = []
 
         exp_folder = os.path.join(scenario_folder, exp) # specific experiment
         exp_responses = np.load(os.path.join(exp_folder, response_file))
@@ -26,12 +26,11 @@ for scenario in scenarios:
         for i, image in enumerate(exp_responses["image_names"]):
             if image in incorrect_labels[exp]:
                 continue # skip errenous response
-
-            questions.append(exp_responses["vlm_prompt"])
-            answers.append(exp_responses["vlm_responses"][i])
-            filenames.append(scenario + "/" + exp + "/image_data/" + exp_responses["image_names"][i])
-    
-    np.savez(os.path.join(scenario_folder, dataset_file), questions = questions, answers = answers, filenames = filenames)
+            else:
+                questions.append(exp_responses["vlm_prompt"])
+                answers.append(exp_responses["vlm_responses"][i])
+                filenames.append(scenario + "/" + exp + "/image_data/" + exp_responses["image_names"][i])
+        np.savez(os.path.join(exp_folder, dataset_file), questions = questions, answers = answers, filenames = filenames)
 
 
 
